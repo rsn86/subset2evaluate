f392ead16e4c7d5965bb472130cfe768c27e3845
Rodrigo Schmidt Nurmberg <521415+rsn86@users.noreply.github.com>
2025-06-15 23:18:10 -0300

    52-QE4PE: Doc level cost-aware selection

--- experiments/52-qe4pe_doc-level_cost-aware_selection.py
+++ experiments/52-qe4pe_doc-level_cost-aware_selection.py
@@ -4,7 +4,6 @@ import subset2evaluate.utils as utils
 import subset2evaluate.evaluate
 import subset2evaluate.select_subset
 import numpy as np
-import tqdm
 
 data_old_all = list(utils.load_data_qe4pe(cache_fname='gsarti_qe4pe_main').values())
 spa_all_random = []
@@ -13,93 +12,74 @@ spa_all_random = []
 # Cost-aware selection
 # Ref: https://github.com/zouharvi/subset2evaluate?tab=readme-ov-file#advanced-usage
 data_full = data_old_all[0]  # Use the first dataset as the full data
-print("Cost-aware selection")
-print(data_full[0]['cost'])
+# print("Cost-aware selection")
+# print(data_full[0]['cost'])
 
 # run basic selection
 # The only non aggregated segment level metrics available are pe_xcomet_qe and qa_pe_esa_rating
-data_new = subset2evaluate.select_subset.basic(data_full, method="metric_var", metric="pe_xcomet_qe")
+data_new = subset2evaluate.select_subset.basic(data_full, method="metric_avg", metric="pe_xcomet_qe")
 
 cost_first_23 = sum([line["cost"] for line in data_new[:23]])
-print(f"Cost for the first 23 items: {cost_first_23:.2f}")
+print(f"Cost for the first 23 segments: {cost_first_23:.2f}")
 
 corr_first_23 = subset2evaluate.evaluate.eval_subset_correlation(data_new[:23], data_full, metric="pe_xcomet_qe")
-print(f"Correlation for the first 23 items: {corr_first_23:.2f}")
+print(f"Correlation for the first 23 segments: {corr_first_23:.2f}")
 
+print("-"* 80)
 # let's run cost-aware selection
 data_costaware = subset2evaluate.select_subset.costaware(data_new, budget=cost_first_23, metric="pe_xcomet_qe")
 
 cost_costaware = sum([line["cost"] for line in data_costaware])
-print(f"Cost of the cost-aware selection: {cost_costaware:.2f}")
+print(f"Cost of the cost-aware selection: {cost_costaware:.2f} #{len(data_costaware)} segsments")
 
 corr_costaware = subset2evaluate.evaluate.eval_subset_correlation(data_costaware, data_full, metric="pe_xcomet_qe")
 print(f"Correlation of the cost-aware selection: {corr_costaware:.2f}")
 
 # %%
 
-# experiments/24-document_level.py
-for repetitions, method_kwargs in [
-    (10, dict(method="random")),
-    # (1, dict(method="metric_avg", metric="MetricX-23-c")),
-    # (1, dict(method="metric_var", metric="MetricX-23-c")),
-    # (1, dict(method="metric_cons", metric="MetricX-23-c")),
-    # (1, dict(method="diversity", metric="lm")),
-    # (5, dict(method="pyirt_diffdisc", metric="MetricX-23-c", model="4pl_score", retry_on_error=True)),
-    # (1, dict(method="precomet_avg")),
-    # (1, dict(method="precomet_var")),
-    # (1, dict(method="precomet_cons")),
-    # (1, dict(method="precomet_diversity")),
-    # (1, dict(method="precomet_diffdisc_direct")),
-]:
-    load_model = None
-    spa_all = []
-    for data_old in tqdm.tqdm(data_old_all):
-        for _ in range(repetitions):
-            def evaluate_aggregate_doc(data_scored):
-                data_old_aggregated = collections.defaultdict(list)
-                for line in data_scored:
-                    data_old_aggregated[line["doc"]].append(line)
 
-                data_old_aggregated = [
-                    {
-                        "doc": doc,
-                        "i": [line["i"] for line in lines],
-                        # average the utilities across the document
-                        "subset2evaluate_utility": np.average([line["subset2evaluate_utility"] for line in lines]),
-                        # average the cost across the document
-                        "cost": np.average([line["cost"] for line in lines])
-                    }
-                    for doc, lines in data_old_aggregated.items()
-                ]
-                data_old_aggregated.sort(key=lambda x: x["subset2evaluate_utility"], reverse=True)
-                data_new_flat = [
-                    data_old[i]
-                    for doc in data_old_aggregated
-                    for i in doc["i"]
-                ]
-                return subset2evaluate.evaluate.eval_spa(data_new_flat, data_old, metric="pe_xcomet_qe")
+def aggregate_doc_score(doc, agg_method=np.average, metric="human"):
+    sys_scores = collections.defaultdict(list)
+    """Aggregate the scores of a document."""
+    for line in doc:
+        for sys,scores in line['scores'].items():
+            if metric not in scores:
+                raise ValueError(f"ERROR: {metric} not found in {sys} scores for {line['i']}")
+            sys_scores[sys].append(scores[metric])
 
-            data_y, load_model = subset2evaluate.select_subset.basic(
-                data_old,
-                **method_kwargs,
-                load_model=load_model if method_kwargs["method"] != "pyirt_diffdisc" else None,
-                return_model=True
-            )
-            spa_all.append(evaluate_aggregate_doc(data_y))
-            if method_kwargs["method"] == "random":
-                spa_all_random.append(spa_all[-1])
-
-    print(method_kwargs["method"], f"{np.average(spa_all):.1%}")
+    return {sys: {metric: agg_method(scores)} for sys, scores in sys_scores.items()}
 
 
-# %%
 
-import subset2evaluate.utils
+def aggregate_doc(data_scored, agg_method=np.sum, metric="human"):
+    """Aggregate the scored data by document."""
+    data_aggregated = collections.defaultdict(list)
+    for line in data_scored:
+        data_aggregated[line["doc"]].append(line)
 
-spa_all_random_arr = np.array(spa_all_random).mean(axis=1).reshape(repetitions, -1).mean(axis=0)
-conf = subset2evaluate.utils.confidence_interval(spa_all_random_arr, confidence=0.90,)
-print(f"{(conf[1]-conf[0])/2:.2%}")
+    data_aggregated = [
+        {
+            "doc": doc,
+            "i": lines[0]["i"],  # keep just the first index for the document
+            # sum the utilities across the document
+            "subset2evaluate_utility": agg_method([line["subset2evaluate_utility"] for line in lines]),
+            # sum the cost across the document
+            "cost": agg_method([line["cost"] for line in lines]),
+            "scores": aggregate_doc_score(lines, agg_method=np.average, metric=metric),
+        }
+        for doc, lines in data_aggregated.items()
+    ]
+    data_aggregated.sort(key=lambda x: x["subset2evaluate_utility"], reverse=True)
+    return data_aggregated
 
-# %%
+print("-"* 80)
+data_aggregated = aggregate_doc(data_new, metric="pe_xcomet_qe")
 
-### TODO: Document-level cost-aware selection
+# let's run cost-aware selection
+data_aggregated_costaware = subset2evaluate.select_subset.costaware(data_aggregated, budget=cost_first_23, metric="pe_xcomet_qe")
+
+cost_aggregated_costaware = sum([line["cost"] for line in data_aggregated_costaware])
+print(f"Cost of the doc aggregated cost-aware selection: {cost_aggregated_costaware:.2f} #{len(data_aggregated_costaware)} documents")
+
+corr_aggregated_costaware = subset2evaluate.evaluate.eval_subset_correlation(data_aggregated_costaware, data_full, metric="pe_xcomet_qe")
+print(f"Correlation of the doc aggregated cost-aware selection: {corr_aggregated_costaware:.2f}")
